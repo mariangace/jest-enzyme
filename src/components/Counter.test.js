099f8d50b283c57f5b02f16d4dc017b0acde8c84
mariangace <mariangace@gmail.com>
2021-05-19 14:27:17 -0500

    click event listener test

--- src/components/Counter.test.js
+++ src/components/Counter.test.js
@@ -1,7 +1,7 @@
 //React
 import React from 'react';
 //Shallow will simulate a component rendering
-import { shallow, Shallow } from 'enzyme';
+import { shallow } from 'enzyme';
 //The component we want to test
 import Counter from './Counter';
 
@@ -10,7 +10,7 @@ describe('Basic test in Counter Component', () => {
   //Test1
   test('should print expected h2 with default number prop', () => {
     //Expected h2
-    const expectedH2 = `The number that comes from the props is: ${20}`;
+    const expectedH2 = `The number that comes from the props is: ${15}`;
 
     //Simulation of the component rendering (with no props)
     const wrapper = shallow(<Counter />);
@@ -24,7 +24,7 @@ describe('Basic test in Counter Component', () => {
   //Test2
   test('Should print expected h2 with number prop', () => {
     //Number that we will sent as number prop
-    const sentProp = 25;
+    const sentProp = 15;
     //Expected h2
     const h2 = `The number that comes from the props is: ${sentProp}`;
     //simulation of the component rendering (with number prop)
@@ -32,5 +32,36 @@ describe('Basic test in Counter Component', () => {
     const foundH2 = wrapper.find('h2').text();
     //COmparision of values
     expect(foundH2).toBe(h2);
+  });
+
+  //Test3
+  test('Should subtract 1 by clicking -button', () => {
+    //Expected value after clicking (15- 1 == 4)
+    const expectedValue = '14';
+    //Simulation of the component rendering (with no prop)
+    const wrapper = shallow(<Counter />);
+    //Button that will be clicked
+    const addButton = wrapper.find('button').at(0);
+    //Simulate click event
+    addButton.simulate('click');
+    //Found value after clickig
+    const foundValue = wrapper.find('p').text();
+    //Comparison of values
+    expect(foundValue).toBe(expectedValue);
+  });
+
+  //Test4
+  test('Should add 1 by clicking + button', () => {
+    //Expected value after clicking (15 + 1 = 16)
+    const expectedValue = '16';
+    //Simulation of the component rendering;
+    const wrapper = shallow(<Counter />);
+    //Button that will be clicked
+    const addButton = wrapper.find('button').at(1);
+    //Simulate click event
+    addButton.simulate('click');
+    //Found value after clicking
+    const foundValue = wrapper.find('p').text();
+    expect(foundValue).toBe(expectedValue);
   });
 });
